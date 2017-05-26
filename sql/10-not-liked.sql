6cfc1dd140240fcb646983fcb43cc59d8a28d695
Maighdlyn <emmamaighdlyn@gmail.com>
2017-05-26 09:19:00 -0700

    Completes day4 exercise 10

---
+++ sql/10-not-liked.sql
@@ -0,0 +1,10 @@
+SELECT student.name AS name, student.grade AS grade
+	FROM student
+	WHERE student.id NOT IN (
+		SELECT likee_id
+		FROM student_like
+		UNION ALL
+		SELECT liker_id
+		FROM student_like)
+	ORDER BY student.name
+;
